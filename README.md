1c2a4b1c90cf59371f3bddae13a1d18bd0ac9655
hengg <wangliang826@hotmail.com>
2020-03-26 21:05:37 +0800

    feat:增加停用检查命令

--- README.md
+++ README.md
@@ -5,4 +5,11 @@
 npm i commit-msg-checker -D
 ```
 ## usage
-在工程根目录下执行`checker-init`
+在工程根目录下执行`checker-init`即可为项目添加检查
+
+如果不再使用，在工程根目录下执行`checker-init del`即可停用检查
+
+## TODO
+- 扩展校验规则
+- 自定义校验规则
+- 自定义配置
--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 #!/usr/bin/env node
 var fs = require("fs");
 
-fs.stat(".git/hooks/", function(err) {
-  if (err) {
-    console.error(err);
-    console.error("请确认当前项目是否通过git管理源代码");
-  } else {
-    var commitMsg = fs.readFileSync(
-      "./node_modules/commit-msg-checker/commit-msg.js"
-    );
-    fs.writeFile(".git/hooks/commit-msg", commitMsg, function(err) {
-      if (err) {
-        console.err(err);
-      } else {
-        console.log("已为项目添加对commit msg的检查");
-        console.log("Happy Hacking!");
-      }
-    });
-  }
-});
+var args = process.argv.splice(2);
+if (args[0] === "del") {
+  fs.access(".git/hooks/commit-msg", function(err) {
+    if (err) {
+      console.error("该项目似乎没有使用commit-msg-checker");
+    } else {
+      var commitMsg = fs.readFileSync(".git/hooks/commit-msg");
+      fs.writeFileSync(".git/hooks/commit-msg.bak", commitMsg);
+      fs.unlink(".git/hooks/commit-msg", function(err) {
+        if (err) console.error(err);
+        console.log("已为项目删除对commit msg的检查🦢");
+      });
+    }
+  });
+} else {
+  fs.stat(".git/hooks/", function(err) {
+    if (err) {
+      console.error(err);
+      console.error("请确认当前项目是否通过git管理源代码");
+    } else {
+      var commitMsg = fs.readFileSync(
+        "./node_modules/commit-msg-checker/commit-msg.js"
+      );
+      fs.writeFile(".git/hooks/commit-msg", commitMsg, function(err) {
+        if (err) {
+          console.err(err);
+        } else {
+          console.log("已为项目添加对commit msg的检查");
+          console.log("Happy Hacking!");
+        }
+      });
+    }
+  });
+}
