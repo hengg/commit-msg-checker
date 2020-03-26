70dd76f3206df962650221de53f1673e666d1185
wangliang16 <wangliang16@58.com>
2020-03-26 15:08:00 +0800

    feat:扩展正则

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # commit-msg-checker
+校验git commit message 是否符合规范，需要Node v8.9及以上版本
+## install
+```bash
+npm i commit-msg-checker -D
+```
+## usage
+在工程根目录下执行`checker-init`
--- commit-msg.js
+++ commit-msg.js
@@ -1,40 +1,40 @@
 #!/usr/bin/env node
 "use strict";
+
 var fs = require("fs");
 
-var REG = /^((feat|fix|chore)(\w*))(\s*)(.*)$/;
+var REG = /^((feat|fix|style|docs|refactor|test|chore)(\(|:)(\w*))(\s*)(.*)$/;
 
 var validateMessage = function(message) {
   var match = REG.exec(message);
   if (!match) {
     console.error(`提交信息：${message} 不符合规范`);
+    console.error("如下规则请参考");
+    console.error("feat:新功能");
+    console.error("fix:修复缺陷");
+    console.error("style:格式化代码");
+    console.error("docs:修改文档");
+    console.error("refactor:重构");
+    console.error("test:增加测试");
+    console.error("chore:构建过程或辅助工具的变动");
     return false;
   }
   return true;
 };
 
-var firstLineFromBuffer = function(buffer) {
+var msgFromBuffer = function(buffer) {
   return buffer
     .toString()
     .split("\n")
     .shift();
 };
 
-exports.validateMessage = validateMessage;
-
 var commitMsgFile = process.argv[2];
-var incorrectLogFile = commitMsgFile.replace(
-  "COMMIT_EDITMSG",
-  "logs/incorrect-commit-msgs"
-);
 
-fs.readFile(commitMsgFile, function(err, buffer) {
-  var msg = firstLineFromBuffer(buffer);
-
+fs.readFile(commitMsgFile, function(_err, buffer) {
+  var msg = msgFromBuffer(buffer);
   if (!validateMessage(msg)) {
-    fs.appendFile(incorrectLogFile, msg + "\n", function() {
-      process.exit(1);
-    });
+    process.exit(1);
   } else {
     process.exit(0);
   }
